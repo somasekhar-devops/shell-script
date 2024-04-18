bff0592b283b33dc97aa4e3a5965eae7ab935fac
somasekhar-devops <somasekhargoudm@gmail.com>
2024-04-18 23:03:42 +0530

    Hello world

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Soma       # Variable declaration (No SPaces between name and =)
+PERSON2=Sekhar
+
+echo "$PERSON1 :: Hi $PERSON2, How are you"
+echo "$PERSON2 :: I am fine How are you"
+echo "$PERSON1 :: I am fine too"
+echo "$PERSON2 :: How is your Job"
