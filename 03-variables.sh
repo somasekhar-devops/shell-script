e48254cb1147a54dfe6d083066e7be5b39caeb21
somasekhar-devops <somasekhargoudm@gmail.com>
2024-04-18 23:08:18 +0530

    Hello world

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1       # Passing values through the arguments to the variables
+PERSON2=$2
+
+echo "$PERSON1 :: Hi $PERSON2, How are you"
+echo "$PERSON2 :: I am fine How are you"
+echo "$PERSON1 :: I am fine too"
+echo "$PERSON2 :: How is your Job"
