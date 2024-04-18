f16ab01fba54b8f5a870ddb7187511654317b662
somasekhar-devops <somasekhargoudm@gmail.com>
2024-04-18 22:58:06 +0530

    Hello world

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh :: Hi Suresh, How are you"
-echo "Suresh :: I am fine How are you"
-echo "Ramesh :: I am fine too"
-echo "Suresh :: How is your Job"
+PERSON1=Ramesh   # Variable declaration (No SPaces between name and =)
+PERSON2=Suresh
+
+echo "$PERSON1 :: Hi $PERSON2, How are you"
+echo "$PERSON2 :: I am fine How are you"
+echo "$PERSON1 :: I am fine too"
+echo "$PERSON2 :: How is your Job"
