38035214e080ca3f12f9722048fa8d72b3de6550
somasekhar-devops <somasekhargoudm@gmail.com>
2024-04-26 23:54:18 +0530

    shell-conditions

--- 09-installation.sh
+++ 09-installation.sh
@@ -10,6 +10,7 @@ USERID=$(id -u)
 if  [ $USERID -ne 0 ]
 then
     echo "You do't have the root aceess, Ask admin to give root access"
+    exit 1 # exit from here if you are not a super user
 else
     echo "You are a super user"
 fi
