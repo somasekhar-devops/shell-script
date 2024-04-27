7414b0a86ac814677d15d095f353324386351cad
somasekhar-devops <somasekhargoudm@gmail.com>
2024-04-27 22:53:45 +0530

    functions shell script

---
+++ 10-functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERROLE=$ (id -u)
+
+if [ $USERROLE -ne 0]
+then
+    echo "You are not a Super User. Get the Super access from admin."
+    exit 1
+else
+    echo "You are a Super User.. Go ahead"
+fi
+
+Validate(){
+    echo "Exit Status of the installation : $1"
+    echo "Completed Task : $2"
+}
+
+dnf install mysql -y
+Validate $? "Installing mysql"
+
