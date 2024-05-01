7f25f3d678b3de524762861d100bcc2232627efe
somasekhar-devops <somasekhargoudm@gmail.com>
2024-05-01 12:57:32 +0530

    calling scripts from other script

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -5,7 +5,9 @@ Course="Variable Value from the Current Script"
 echo "Before calling the Other Script : $Course"
 echo "Process ID Before calling other script : $$"
 
-./15-other-script.sh
+# ./15-other-script.sh
+
+source ./15-other-script.sh
 
 echo "After calling thhe other script : $Course"
 
