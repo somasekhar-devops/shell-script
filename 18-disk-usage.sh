112a7e9919c43c888570b399c30b838af6740507
somasekhar-devops <somasekhargoudm@gmail.com>
2024-05-19 13:20:12 +0530

    disk usage

---
+++ 18-disk-usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Disk_Usage=$(df -hT | grep xfs)
+Disk_Treshold=6
+
+while IFS= read -r line
+do
+    Usage=$(echo $line | awk -F " " '{print $6F}') | cut -d "%" -f1 )
+    Folder=$(echo $line | awk -F " " '{print $NF}' )
+
+    if [ $Usage -ge $Disk_Treshold ]
+    then
+        echo "$Folder is more than $Disk_Threshold, Current usage is : $Usage"
+
+done <<< $Disk_Usage
